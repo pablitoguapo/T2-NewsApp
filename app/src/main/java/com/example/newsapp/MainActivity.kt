b5da321368712750ba7c24429e80b0c4bf009414
pablitoguapo <pmg82260@lasallebajio.edu.mx>
2026-03-11 16:31:44 -0600

    Ultima Actualizacion

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp
 
+import NewsApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,10 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsAppTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    NewsApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/newsapp/NewsApp.kt
+++ app/src/main/java/com/example/newsapp/NewsApp.kt
@@ -10,19 +10,25 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import com.example.newsapp.ui.theme.NewsAppTheme
 
+
+
+data class Noticias(val titulo: String, val imageUrl: String)
+
 @Composable
-fun NewsApp(){
-
-
+fun NewsApp(modifier: Modifier = Modifier){
 
     Column(
         modifier = Modifier
@@ -78,7 +84,8 @@ fun NewsApp(){
             modifier = Modifier.
             padding(bottom = 20.dp),
             text = "Ultimas Noticias",
-            fontSize = 20.sp
+            fontSize = 20.sp,
+            fontWeight = androidx.compose.ui.text.font.FontWeight.Bold
         )
         val Lista = listOf(
             "El presidente de EE.UU. no muestra signos de arrepentimiento...",
@@ -110,8 +117,60 @@ fun NewsApp(){
             }
         }
 
+        //Noticias cartas con imagenes
+        val listaNoticias = listOf(
+            Noticias("El presidente de EE.UU. no muestra signos de arrepentimiento...", "https://static01.nyt.com/images/2025/01/17/espanol/17dc-trump-photo-01-cpvw-ES-copy1/17dc-trump-photo-01-cpvw-jumbo.jpg?quality=75&auto=webp"),
+            Noticias("Bañarse en la piscina del desierto de Cleopatra", "https://static.wikia.nocookie.net/amor-de-otro-mundo/images/b/b0/Cleopatra.png/revision/latest?cb=20221211124811&path-prefix=es"),
+            Noticias("Gigantes tecnológicos", "https://ichef.bbci.co.uk/ace/ws/800/cpsprodpb/9fa5/live/5e7f42c0-20d7-11ee-b2fa-a5b23768df59.jpg.webp"),
+            Noticias("El rover de Marte envía nuevas imágenes", "https://josevicentediaz.com/wp-content/uploads/2014/09/matt.jpg")
+        )
+        Text(
+            modifier = Modifier.
+            padding(bottom = 3.dp, top = 20.dp),
+            text = "Alrededor del mundo"
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(listaNoticias) { noticia ->
+                Box(
+                    modifier = Modifier
+                        .height(200.dp)
+                        .clip(RoundedCornerShape(24.dp))
+                ) {
+
+                    AsyncImage(
+                        model = noticia.imageUrl,
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
 
 
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(8.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(Color.White.copy(alpha = 0.8f))
+                            .padding(10.dp)
+                    ) {
+                        Text(
+                            text = noticia.titulo,
+                            fontSize = 11.sp,
+                            fontWeight = FontWeight.Bold,
+                            maxLines = 3
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
